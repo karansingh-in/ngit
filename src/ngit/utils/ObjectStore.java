a510d0ba35f1edcb1a38dbd7b7cd895681cc2057
Karan Singh <singh.karangkp@gmail.com>
2026-08-08 14:35:25 +0530

    feat: replaced hardcoded paths with dynamic ones

--- src/ngit/utils/ObjectStore.java
+++ src/ngit/utils/ObjectStore.java
@@ -1,15 +1,17 @@
 package ngit.utils;
 
+import ngit.repository.Repository;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 
 public class ObjectStore {
     public static String store(byte[] data) throws NoSuchAlgorithmException, IOException {
         String hash = HashUtil.generateHash(data);
-        Path path = Path.of("../../.ngit/objects", hash);
+        Repository repo = new Repository();
+        repo.findRepository();
+        Path path = Path.of(repo.getObjects().toString(), hash);
 
         if (!Files.exists(path)) {
             byte[] compressed = CompressUtil.compress(data);
