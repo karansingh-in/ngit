b31225aa1abb49fee647c73c865fcfba908412b1
Karan Singh <singh.karangkp@gmail.com>
2026-08-09 01:25:21 +0530

    feat: implemented init command to create the .ngit repo and initialize HEAD

---
+++ src/ngit/commands/InitCommand.java
@@ -0,0 +1,16 @@
+package ngit.commands;
+
+import ngit.repository.Repository;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class InitCommand {
+    private Repository repo;
+    public InitCommand(Repository repo){
+        this.repo = repo;
+    }
+    public void initialize() throws IOException {
+        repo.initialize();
+        Files.writeString(repo.getHEAD(), "refs: refs/heads/main");
+    }
+}
