a9004377dbc11ae608a7afa31740b095c0f2a6e5
Karan Singh <singh.karangkp@gmail.com>
2026-08-09 00:37:56 +0530

    refactor: changed HashMap type from String, String to Path, String for ease of implementation

--- src/ngit/utils/Index.java
+++ src/ngit/utils/Index.java
@@ -3,19 +3,20 @@ package ngit.utils;
 import ngit.repository.Repository;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Index {
-    private HashMap<String, String> map = new HashMap<>();
+    private HashMap<Path, String> map = new HashMap<>();
     private Repository repo;
 
-    Index(Repository repo){
+    public Index(Repository repo){
         this.repo = repo;
     }
 
-    public void put(String hash, String path){
+    public void put(Path path, String hash){
         map.put(path, hash);
     }
 
@@ -23,13 +24,13 @@ public class Index {
         List<String> lines = Files.readAllLines(repo.getIndex());
         for (String line : lines){
             String[] pair = line.split(",");
-            map.put(pair[0], pair[1]);
+            map.put(Path.of(pair[0]), pair[1]);
         }
     }
 
     public void save() throws IOException {
         List<String> lines = new ArrayList<>();
-        for (String key : map.keySet()){
+        for (Path key : map.keySet()){
             lines.add(key +","+ map.get(key));
         }
         Files.write(repo.getIndex(), lines);
