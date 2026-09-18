a85abe70682d04e0c44d3ebed3ea653a35b52e33
Karan Singh <singh.karangkp@gmail.com>
2026-09-18 23:15:29 +0530

    my personal touch

---
+++ src/ngit/commands/EasterEggCommand.java
@@ -0,0 +1,15 @@
+package ngit.commands;
+
+public class EasterEggCommand {
+    public static void myPersonalTouch(){
+        System.out.println("""
+                HEYYYYYY!!!
+                
+                Congrats on finding this hidden section! I really appreciate you looking under the hood this deep.
+                This is me at the time of compiling the first version of this software.
+                
+                I think you must try Elden Ring at least once.
+                ping me at: karannsinggh@gmail.com let me know you were here :D
+                """);
+    }
+}
